b7419632448677a005031ba950943a511747d516
Marco Nitola <mnitolabaro@uniminuto.edu.co>
2023-12-05 20:17:56 -0500

    new key

--- controllers/openAiController.js
+++ controllers/openAiController.js
@@ -9,7 +9,7 @@ const APIKEY = process.env.OPEN_AI_API_KEY;
 
 
 const openai = new OpenAI({
-    apiKey: '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',
+    apiKey: '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',
   });
 
   //generar imagen
